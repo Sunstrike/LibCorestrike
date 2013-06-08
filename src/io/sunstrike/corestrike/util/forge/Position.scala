9067efe9a8496a5ba0d6c6476bc9bef0a744e87d
Sunstrike <sunstrike@azurenode.net>
2013-06-08 15:12:54 +0100

    Position util class for Forge

---
+++ src/io/sunstrike/corestrike/util/forge/Position.scala
@@ -0,0 +1,30 @@
+package io.sunstrike.corestrike.util.forge
+
+import net.minecraft.world.World
+import net.minecraftforge.common.ForgeDirection
+import scala.ref.WeakReference
+
+/*
+ * Position
+ * io.sunstrike.corestrike.util.forge
+ */
+
+/**
+ * Utility class to store location and optional ForgeDirection.
+ *
+ * @author Sunstrike
+ */
+class Position(w:World, xPos:Int, yPos:Int, zPos:Int, d:ForgeDirection) {
+
+    def this(w:World, xPos:Int, yPos:Int, zPos:Int) = this(w, xPos, yPos, zPos, ForgeDirection.UNKNOWN)
+
+    /**
+     * This doesn't return a World! .apply() must be called to retrieve the world.
+     */
+    val world = new WeakReference(w)
+    val x = xPos
+    val y = yPos
+    val z = zPos
+    val dir = d
+
+}
